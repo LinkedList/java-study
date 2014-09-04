329ce91533d76108a5d762c69f8d3fe1252c3748
Macko Martin <macko.mar@gmail.com>
2014-09-04 12:04:28 +0200

    removed unused import on User

--- src/main/java/martin/models/entities/User.java
+++ src/main/java/martin/models/entities/User.java
@@ -5,7 +5,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotEmpty;
 
