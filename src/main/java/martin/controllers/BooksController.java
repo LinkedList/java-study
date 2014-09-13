3d86482a5d3975458d4ab8b1d503f9881f138094
linkedlist <macko.mar@gmail.com>
2014-09-13 09:27:18 +0200

    book creation under user

--- src/main/java/martin/controllers/BooksController.java
+++ src/main/java/martin/controllers/BooksController.java
@@ -1,13 +1,20 @@
 package martin.controllers;
 
 import java.util.List;
+import javax.validation.Valid;
 import martin.models.entities.Book;
+import martin.models.entities.User;
 import martin.models.managers.BookManager;
+import martin.models.managers.UserManager;
 import martin.models.seeders.BooksSeeder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  *
@@ -19,6 +26,9 @@ public class BooksController {
 
 	@Autowired
 	private BookManager bookManager;
+
+	@Autowired
+	private UserManager userManager;
 
 	@Autowired
 	private BooksSeeder booksSeeder;
@@ -39,4 +49,27 @@ public class BooksController {
 		return "books/index";
 	}
 	
+	@RequestMapping(value="/create/{userId}", method=RequestMethod.GET)
+	public String bookCreate(@PathVariable("userId") Long userId, Model model) {
+		Book book = new Book();
+
+		model.addAttribute("book", book);
+		model.addAttribute("userId", userId);
+		return "books/bookCreate";
+	}
+
+	@RequestMapping(value="/create/{userId}", method=RequestMethod.POST)
+	public String bookCreatePost(@PathVariable("userId") Long userId, @ModelAttribute @Valid Book book, BindingResult result, Model model) {
+
+		if(result.hasErrors()) {
+			return "books/bookCreate";
+		}	
+
+		User user = userManager.findById(userId);
+		book.setUser(user);
+
+		bookManager.saveOrUpdate(book);
+
+		return "redirect:/users/user/" + userId;
+	}
 }
---
+++ src/main/webapp/WEB-INF/views/books/bookCreate.jsp
@@ -0,0 +1,28 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>
+<%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions"%>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<%@taglib prefix="t" tagdir="/WEB-INF/tags"%>
+<%@ page isELIgnored="false"%>
+<t:wrapper>
+	<div class="container">
+		<h1>Create a new awesome book</h1>
+
+		<c:url var="postUrl" value="/books/create/${userId}" />
+		<form:form method="POST" commandName="book" role="form" action="${postUrl}">
+			<div class="form-group">
+				<label for="title">Title:</label>
+				<form:input path="title" class="form-control" id="title" />
+				<form:errors path="title" />
+			</div>
+			<div class="form-group">
+				<label for="description">Description:</label>
+				<form:input path="description" class="form-control" id="description" />
+				<form:errors path="description" />
+			</div>
+				<form:button type="submit" value="Save" class="btn btn-primary">Save</form:button>
+		</form:form>
+	</div>
+</t:wrapper>
