a4e401597260e6c1000ec5fca955f5fd26fcce31
linkedlist <macko.mar@gmail.com>
2014-09-20 16:30:50 +0200

    removed unused SessionStatus

--- src/main/java/martin/controllers/UsersController.java
+++ src/main/java/martin/controllers/UsersController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
-import org.springframework.web.bind.support.SessionStatus;
 
 @Controller
 @RequestMapping(value = "/users/")
@@ -84,7 +83,7 @@ public class UsersController {
 
 
 	@RequestMapping(value="/createOrEdit", method=RequestMethod.POST)
-	public String userCreateOrEditPost(@ModelAttribute("command") @Valid UserCommandObject command, BindingResult result, Model model, SessionStatus status) {
+	public String userCreateOrEditPost(@ModelAttribute("command") @Valid UserCommandObject command, BindingResult result, Model model) {
 		if(result.hasErrors()) {
 			return "users/userCreateOrEdit";
 		}
